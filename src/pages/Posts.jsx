b6618d00d9077133493fdfaa1a0dcac058e16993
prasad-suku <142576212+prasad-suku@users.noreply.github.com>
2024-07-24 07:45:27 -0400

    Add files via upload

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import { useParams,Link } from 'react-router-dom'
+import React from "react";
+import { useParams, Link } from "react-router-dom";
 
 function Posts() {
-    let{id,name}=useParams()
-    console.log(id);
+  let { id, name } = useParams();
+  console.log(id);
   return (
     <>
-    <h2>Post :{id} </h2>
-     <p>klf;djsl;fkasd</p>
-     
+      <h2>Post :{id} </h2>
+      <p>klf;djsl;fkasd</p>
     </>
-  )
+  );
 }
 
-export default Posts
+export default Posts;
--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React from "react";
 
 function Service() {
   return (
     <>
-    <h1>Service</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis reprehenderit earum cupiditate deleniti minima nulla similique, tenetur odit id rerum unde iste fugiat magni atque voluptatum, ducimus officiis sint commodi? Possimus unde dignissimos maxime? At impedit minus nam numquam cumque eligendi, atque porro repellendus optio minima veniam nihil hic exercitationem debitis excepturi alias dolore non ducimus. Voluptatum iusto optio quas! Amet harum illum sunt fugit asperiores quas vitae magnam consectetur delectus consequatur voluptatibus, adipisci nesciunt vel ut esse est explicabo ab? Ratione totam eaque laudantium eius sapiente deserunt earum aliquam accusantium quam tempora inventore vel et officia ipsum, corrupti nesciunt harum dicta. Iure est incidunt nobis blanditiis. Facilis dolores commodi eligendi magnam. At quod voluptates dolor inventore. Molestias autem sapiente vel odio laborum, blanditiis in quae consequuntur deleniti totam tempore error libero est velit cupiditate placeat consequatur exercitationem, necessitatibus ullam aliquam. Ipsam maiores accusamus voluptatem iusto quam, veritatis fugit adipisci architecto. Assumenda dolores sed id totam quisquam? Quasi, voluptatum libero quos ipsum saepe cupiditate dicta necessitatibus recusandae nobis tempore quidem inventore laborum? Aspernatur nesciunt ut voluptas, nulla tenetur cumque rerum recusandae non expedita ipsum dolores atque omnis consequuntur animi iusto eos doloribus similique sint ipsa perspiciatis sit dicta asperiores eveniet.</p>
+      <h1>Service</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+        reprehenderit earum cupiditate deleniti minima nulla similique, tenetur
+        odit id rerum unde iste fugiat magni atque voluptatum, ducimus officiis
+        sint commodi? Possimus unde dignissimos maxime? At impedit minus nam
+        numquam cumque eligendi, atque porro repellendus optio minima veniam
+        nihil hic exercitationem debitis excepturi alias dolore non ducimus.
+        Voluptatum iusto optio quas! Amet harum illum sunt fugit asperiores quas
+        vitae magnam consectetur delectus consequatur voluptatibus, adipisci
+        nesciunt vel ut esse est explicabo ab? Ratione totam eaque laudantium
+        eius sapiente deserunt earum aliquam accusantium quam tempora inventore
+        vel et officia ipsum, corrupti nesciunt harum dicta. Iure est incidunt
+        nobis blanditiis. Facilis dolores commodi eligendi magnam. At quod
+        voluptates dolor inventore. Molestias autem sapiente vel odio laborum,
+        blanditiis in quae consequuntur deleniti totam tempore error libero est
+        velit cupiditate placeat consequatur exercitationem, necessitatibus
+        ullam aliquam. Ipsam maiores accusamus voluptatem iusto quam, veritatis
+        fugit adipisci architecto. Assumenda dolores sed id totam quisquam?
+        Quasi, voluptatum libero quos ipsum saepe cupiditate dicta
+        necessitatibus recusandae nobis tempore quidem inventore laborum?
+        Aspernatur nesciunt ut voluptas, nulla tenetur cumque rerum recusandae
+        non expedita ipsum dolores atque omnis consequuntur animi iusto eos
+        doloribus similique sint ipsa perspiciatis sit dicta asperiores eveniet.
+      </p>
     </>
- 
-  )
+  );
 }
 
-export default Service
+export default Service;
